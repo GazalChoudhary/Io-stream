a5dfb3e9a3d3454521f70d7dcd36bbf5619e6517
Gazal Choudhary <47411483+GazalChoudhary@users.noreply.github.com>
2019-02-25 21:31:53 -0800

    Add files via upload

---
+++ First.java
@@ -0,0 +1,34 @@
+package Thread;
+
+ class First {
+//synchronized 
+	 public void display(String msg)
+	 {
+		System.out.print("["+msg);
+		try {
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+			
+		}
+		System.out.println("]");
+	}
+}
+class Second extends Thread{
+	String msg;
+	First fobj;
+	
+	Second(First fp,String str){
+		fobj=fp;
+		msg=str;
+		start();
+		}
+	public void run()
+	{	
+		synchronized (fobj) {
+			fobj.display(msg);
+		}
+		
+	}
+}
